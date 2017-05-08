ebcfc6d31e7612a0e1a0c371f5b1bf9ec25bca0e
Schalk van Heerden <vhschalk@gmail.com>
2017-05-08 23:36:38 +0200

    Add year tag

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ While the TheAudioDB music video scrapper is broken for the Kodi (OSMC) media ce
 
 Open kodimvnfo.py with a text editor and change the <i>mypath</i> variable to your main music video directory.
 
-File should be renamed in the format <i>Artist name - Song name (Extra info)</i>. <i>(Extra info)</i> is an optional parameter.
+File should be renamed in the format <i>Artist Name - Song Name (Extra Info)</i>. <i>(Extra Info)</i> is optional.
+
+A date parameter can be provided in <i>Extra Info</i> in the format yymmdd or yyyy. The code will automatically find the date entry if multiple parameters is added to <i>Extra Info</i>
 
 Run in terminal >> python kodimvnfo.py
 
--- kodimvnfo.py
+++ kodimvnfo.py
@@ -11,9 +11,21 @@ def mvnfo(directory):
             if s < 0:
                 s = len(filename)
                 info = ""
+                year = ""
                 s = m+1
             else:
                 info = filename[s+1:m-1]
+
+                date = [int(d) for d in info.split() if d.isdigit()]
+                if len(date) > 0:
+                    date.sort(reverse=True)
+                    if date[0] >= 100000: #yymmdd
+                        year = "20" + str(date[0])[0:2]
+                    elif date[0] >= 1000: #yyyy
+                        year = str(date[0])
+                else:
+                    year = ""
+
             if a > 0:
                 artist = filename[0:a-1]
                 song = filename[a+2:s-1]
@@ -22,11 +34,12 @@ def mvnfo(directory):
                 f.write("<musicvideo>\n")
                 f.write("<title>" + song + "</title>\n")
                 f.write("<artist>" + artist + "</artist>\n")
+                f.write("<year>" + year + "</year>\n")
                 f.write("<plot>" + info + "</plot>\n")
                 f.write("</musicvideo>")
                 f.close()
-                print filename[0:m] + '.nfo'
+                print(filename[0:m] + '.nfo ' + year)
 
-mypath="/home/user/Videos/"
+mypath = "D:\Videos\MK Videos"
 
 mvnfo(mypath)
